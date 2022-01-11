e51c4fb91ad053a95c04cb2e72dba853831d7ade
AveryCohen <AveryCohen04@gmail.com>
2022-01-11 11:00:05 -0500

    started carTrip

--- src/CarTrip.java
+++ src/CarTrip.java
@@ -1,3 +1,52 @@
 public class CarTrip {
+    // instance variables
+    private int myStartOdometer;
+    private int myEndOdometer;
+    private double myTime;
+    private double gallonsUsed;
 
+
+    //constructors
+    public CarTrip () {
+        myStartOdometer = 0;
+        myEndOdometer = 0;
+        myTime = 0.0;
+        gallonsUsed = 0.0;
+    }
+    public CarTrip (int start, int end, double time, double gallons) {
+        myStartOdometer = start;
+        myEndOdometer = end;
+        myTime = time;
+        gallonsUsed = gallons;
+
+    }
+
+    //methods - getters and setters first
+    public int getMyStartOdometer() {
+        return myStartOdometer;
+    }
+    public int getMyEndOdometer() {
+        return myEndOdometer;
+    }
+    public int getMyTime() {
+        return myTime;
+    }
+    public int getGallonsUsed() {
+        return gallonsUsed;
+    }
+    public void setMyStartOdometer(int value) {
+        myStartOdometer = value;
+    }
+
+    public void setMyEndOdometer(int myEndOdometer) {
+        this.myEndOdometer = myEndOdometer;
+    }
+
+    public void setMyTime(double myTime) {
+        this.myTime = myTime;
+    }
+
+    public void setGallonsUsed(double gallonsUsed) {
+        this.gallonsUsed = gallonsUsed;
+    }
 }
