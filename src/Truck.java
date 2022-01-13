dbe47060f80d4a0e975ad497083cdc8158c40b31
AveryCohen <AveryCohen04@gmail.com>
2022-01-13 14:30:22 -0500

    started truck

--- src/Truck.java
+++ src/Truck.java
@@ -1,3 +1,57 @@
 public class Truck {
+    private String truckID;
+    private double odometer;
+    private double mpg;
+    private double fuel;
+    private final double CAPACITY = 20.0;
+    private static double totalFuel;
+
+    Truck(String a) {
+        truckID = a;
+        odometer = 0.0;
+        mpg = 0.0;
+        fuel = 0.0;
+    }
+
+    Truck(String b, double c, double d, double e) {
+        truckID = b;
+        odometer = c;
+        mpg = d;
+        fuel = e;
+    }
+    public String getTruckID() {
+        return truckID;
+    }
+    public double getOdometer() {
+        return odometer;
+    }
+    public double getMpg() {
+        return mpg;
+    }
+    public double getFuel() {
+        return fuel;
+    }
+    public void setMpg(double m) {
+        mpg = m;
+    }
+
+    public boolean enoughFuel(double miles) {
+        double range = mpg * fuel;
+        if (range >= miles) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public int drive(double miles) {
+        if (enoughFuel(miles)== false) {
+            return 0;
+        }
+        else {
+            fuel -= ((mpg*fuel)-miles)/mpg;
+        }
+    }
+
 
 }
