7ca7ed3477f49cb9175e0a14e0c3abb8b4baf1a8
AveryCohen <AveryCohen04@gmail.com>
2022-01-19 09:00:34 -0500

    started fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,23 @@
 public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    Fraction(int n, int d) {
+
+        if ((Math.abs(n) != n && Math.abs(d) == d) || (Math.abs(d) != d && Math.abs(n) == n)) {
+            numerator = Math.abs(n) * -1;
+            denominator = Math.abs(d);
+        }
+        else {
+            numerator = Math.abs(n);
+            denominator = Math.abs(d);
+        }
+
+    }
+    public int gcd(int a, int m) {
+
+    }
+
 
 
 }
